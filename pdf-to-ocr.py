bb1f004a84288a78eae6848c0098286fee4885c2
YASMIN PACHECO GIL BONILHA <23.01425-3@maua.br>
2024-04-26 12:44:24 -0300

    tentativa 02

--- pdf-to-ocr.py
+++ pdf-to-ocr.py
@@ -1,5 +1,4 @@
 from pdf2image import convert_from_path
-import cv2
 import numpy as np
 import pandas as pd
 import pytesseract
@@ -7,23 +6,23 @@ import fitz  # PyMuPDF, imported as fitz for backward compatibility reasons
 
 
 #corrige a orientação das imagens - tem como parametro a imagem e retorna distorcida
-def deskew(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
-    coords = np.column_stack(np.where(gray > 0))
-    angle = cv2.minAreaRect(coords)[-1]
+# def deskew(image):
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     gray = cv2.bitwise_not(gray)
+#     coords = np.column_stack(np.where(gray > 0))
+#     angle = cv2.minAreaRect(coords)[-1]
     
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
+#     if angle < -45:
+#         angle = -(90 + angle)
+#     else:
+#         angle = -angle
 
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+#     (h, w) = image.shape[:2]
+#     center = (w // 2, h // 2)
+#     M = cv2.getRotationMatrix2D(center, angle, 1.0)
+#     rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-    return rotated
+#     return rotated
 
 
 #realiza a ocr na imagem passada como parâmetro e retorna o texto extraído no formato de uma string.
@@ -35,7 +34,7 @@ def extract_text_from_image(image):
 #implementação
 #passar o pdf como imagem
 def pdf_to_img(pdf):
-    file_path = "my_file.pdf"
+    file_path = r"C:\Users\23.01425-3\Documents\pi-pdf\pi-pdf\Documento1.pdf"
     doc = fitz.open(file_path)  # open document
     for i, page in enumerate(doc):
         pix = page.get_pixmap()  # render page to an image
@@ -47,7 +46,7 @@ def pdf_to_img(pdf):
 extracao = []
 pdf_file = r"C:\Users\bonil\Documents\faculdade\terceiro semestre\pi-python-test\Info sobre o pi.pdf"
 image = pdf_to_img(pdf_file)
-preprocessed_image = deskew(image)
+preprocessed_image = image
 text = extract_text_from_image(preprocessed_image)
 extracao.append(text)
 print(extracao)
