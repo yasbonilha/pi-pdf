7c62414318a38511754ff50b995034bfc1684383
yasmin bonilha <bonilhayaya@gmail.com>
2024-05-06 11:56:52 -0300

    reconhecimento de texto através de imagens e camera

--- pdf-to-ocr.py
+++ pdf-to-ocr.py
@@ -4,6 +4,10 @@ import pandas as pd
 import pytesseract
 import fitz  # PyMuPDF, imported as fitz for backward compatibility reasons
 from PIL import Image
+import cv2
+import os.path
+import random
+
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\bonil\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
@@ -28,13 +32,50 @@ def pdf_to_img(pdf_file):
         
 
 
+type = int(input("1 - escanear\n 2- tirar foto: "))
 
+#escanear
 #lista para armazenar o texto de todas as páginas
-extracao = []
-pdf_file = "Info sobre o pi.pdf"
-image = pdf_to_img(pdf_file)
-preprocessed_image = Image.open(image)
-text = extract_text_from_image(preprocessed_image)
-extracao.append(text)
-print(extracao)
+if type == 1 :
+    extracao = []
+    pdf_file = "Info sobre o pi.pdf"
+    image = pdf_to_img(pdf_file)
+    preprocessed_image = Image.open(image)
+    text = extract_text_from_image(preprocessed_image)
+    extracao.append(text)
+    print(extracao)
+elif type == 2:
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("test")
+    img_name = ""
+
+    control = True
+    while control:
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            control = False
+        cv2.imshow("test", frame)
+
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            control = False
+
+        elif k%256 == 32:
+            # SPACE pressed
+            img_name = "opencv_frame_{}.png".format(random.randint(4, 1000))
+            while os.path.isfile(img_name):
+                img_name = "opencv_frame_{}.png".format(random.randint(5, 1000))
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            control = False
+
+    cam.release()
+    cv2.destroyAllWindows()
+    preprocessed_image = Image.open(img_name)
+    text = extract_text_from_image(preprocessed_image)
+    print(text)
+
 
